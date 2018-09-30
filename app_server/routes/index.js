c76a02ed78e66a3a098a0ba43805307714a2e59e
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2018-09-30 10:02:44 -0500

    routes created and updated

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var ctrlMain = require('../controllers/main');
+const express = require('express');
+const router = express.Router();
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', ctrlMain.Inicio);
+router.get('/', ctrlMain.inicio);
+router.get('/nosotros', ctrlMain.nosotros);
+router.get('/contactanos', ctrlMain.contactanos);
+router.get('/cine', ctrlMain.cine);
+router.get('/animacion', ctrlMain.animacion);
+router.get('/tv', ctrlMain.tvProduccion);
+
 
 module.exports = router;
