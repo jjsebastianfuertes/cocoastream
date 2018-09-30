1a42cc14bebe67920c608c8508e5fc983587f4dd
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2018-09-30 10:02:18 -0500

    controllers info updated basics

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,29 @@
-module.exports.Inicio =  function(req, res) {
+//home page
+module.exports.inicio =  (req, res) => {
     res.render('index', { title: 'COCOA STREAM' });
   }
+
+//nosotros page
+module.exports.nosotros = (req, res) => {
+  res.render('nosotros', {title: 'Nosotros'});
+}
+
+//contactanos page
+module.exports.contactanos = (req, res) => {
+  res.render('contactanos', {title: 'Contactanos'});
+}
+
+//cine page
+module.exports.cine = (req, res) => {
+  res.render('cine', {title: 'Cine'});
+}
+
+//animacion page
+module.exports.animacion = (req, res) => {
+  res.render('animacion', {title: 'Animación'})
+}
+
+//tv page
+module.exports.tvProduccion = (req, res) => {
+  res.render('tv', {title: 'T.V. Producción'})
+}
