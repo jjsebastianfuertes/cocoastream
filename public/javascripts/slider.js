d10e54bc20326f4a28d04ce6edfea0861bb908af
Juan Jose Rueda <pepefeliblu@gmail.com>
2018-10-28 23:23:19 -0500

    slider name changes now

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -12,12 +12,15 @@ function cambiarCat(n) {
 function mostrarCat(n) {
   var i;
   var categoria = document.getElementsByClassName("categoria");
+  let name = document.getElementsByClassName("catName");
   if (n > categoria.length) {catIndex = 1} 
   if (n < 1) {catIndex = categoria.length}
   for (i = 0; i < categoria.length; i++) {
       categoria[i].style.display = "none"; 
+      name[i].style.display = "none";
   }
  
   categoria[catIndex-1].style.display = "block"; 
+  name[catIndex-1].style.display = "block";
   
 }
