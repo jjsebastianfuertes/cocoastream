d4d9a7b2a23849a8ebb7c0d58b6fa9bbfa768cc8
xavgithub <xaviervinueza@hotmail.com>
2018-11-12 08:57:54 -0500

    changes on main corrected

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-//home page
-module.exports.inicio =  (req, res) => {
-    res.render('index', { title: 'COCOA STREAM' });
-  }
-
-//nosotros page
-module.exports.nosotros = (req, res) => {
-  res.render('nosotros', {title: 'Nosotros'});
-}
-
-//contactanos page
-module.exports.contactanos = (req, res) => {
-  res.render('contactanos', {title: 'Contactanos'});
-}
-
-//cine page
-module.exports.cine = (req, res) => {
-  res.render('cine', {title: 'Cine'});
-}
-
-//animacion page
-module.exports.animacion = (req, res) => {
-  res.render('animacion', {title: 'Animación'})
-}
-
-//tv page
-module.exports.tvProduccion = (req, res) => {
-  res.render('tv', {title: 'T.V. Producción'})
-}
-
-//menu page
-module.exports.menu = (req, res) => {
-  res.render('menu')
-}
-
-//admin page
-module.exports.admin = (req, res) => {
-  res.render('admin')
-}
-//leonardo page ... borrar despues de presentacion
-module.exports.leonardo = (req, res) => {
-  res.render('leonardo')
-}
-
-//perfil admin page
-module.exports.perfiladmin = (req, res) => {
-  res.render('perfiladmin')
-}
-//subir video admin page
-module.exports.subirvideo = (req, res) => {
-  res.render('subirvideo')
-}
-//editor admin page
-module.exports.editoradmin = (req, res) => {
-  res.render('editoradmin')
-}
-=======
 //home page
 module.exports.inicio =  (req, res) => {
     res.render('index', { title: 'COCOA STREAM' });
@@ -109,4 +51,3 @@ module.exports.perfiladmin = (req, res) => {
 module.exports.editoradmin = (req, res) => {
   res.render('editoradmin')
 }
->>>>>>> f59540f0dc7c2f5a7c7f0c18a7d3e724e7630afe
