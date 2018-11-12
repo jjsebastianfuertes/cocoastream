72d2fdc9d7741b603d2fd54ebd03303beeb7af8e
Juan Jose Rueda <pepefeliblu@gmail.com>
2018-11-12 01:00:20 -0500

    users route typo fixed - basic controller struct added

--- app_server/controllers/usuarios.js
+++ app_server/controllers/usuarios.js
@@ -4,6 +4,35 @@ var User = mongoose.model('user');
 var sendJsonResponse = function(req, res){
     res.status(status);
     res.json(content);
-}
+};
 
+
+module.exports.usersCreate = function(req, res){
+
+    sendJsonResponse(res, 200,{"status":"success"}); 
+};
+
+module.exports.usersList = function(req, res){
+
+    sendJsonResponse(res, 200,{"status":"success"}); 
+};
+
+module.exports.usersReadOne = function(req, res){
+
+    User
+        .findById(req.params.videoid)
+        .exec(function(err, video){
+            sendJsonResponse(res, 200,video);
+        });
+};
+
+module.exports.usersUpdateOne = function(req, res){
+
+    sendJsonResponse(res, 200,{"status":"success"}); 
+};
+
+module.exports.usersDeleteOne = function(req, res){
+
+    sendJsonResponse(res, 200,{"status":"success"}); 
+};
 
--- app_server/routes/usuarios.js
+++ app_server/routes/usuarios.js
@@ -6,7 +6,7 @@ let ctrlUser = require('../controllers/usuarios')
 router.get('/users', ctrlUser.usersList);
 router.post('/users', ctrlUser.usersCreate);
 router.get('/users/:userid', ctrlUser.usersReadOne);
-router.put('/users/:usersid', ctrlUser.userUpdateOne);
-router.delete('users/:usersid', ctrlUser.userDeleteOne);
+router.put('/users/:usersid', ctrlUser.usersUpdateOne);
+router.delete('users/:usersid', ctrlUser.usersDeleteOne);
 
 module.exports = router;
