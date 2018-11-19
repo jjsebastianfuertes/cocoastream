924c5fcbfaeaa2d7d9c4d2d144f752dd694d1648
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2018-11-19 02:16:46 -0500

    app update

--- app.js
+++ app.js
@@ -5,7 +5,35 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+//var usuario = require('./app_api/schemas/usuarios');
+//var series = 
+//var peliculas = require('./app_api/schemas/peliculas');
 
+
+//mongoose connection 
+require('./app_api/models/db');
+
+
+
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/usuarios');
+
+let routesApi = require('./app_api/routes/index');
+//let usersApi = require('./app_api/routes/users');
+
+
+
+//instantiations
+var app = express();
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
