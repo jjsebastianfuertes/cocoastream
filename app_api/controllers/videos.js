f24ebf27726a969721ee5b6ef7a1478d24dd3fce
Juan Jose Rueda <pepefeliblu@gmail.com>
2018-11-19 22:18:50 -0500

    CRUD complete

--- app_api/controllers/videos.js
+++ app_api/controllers/videos.js
@@ -57,16 +57,108 @@ module.exports.videosReadOne = function (req, res) {
 	Vid
 		.findById(req.params.videoid)
 		.exec(function(err, video){
-			sendJsonResponse(res, 200,video);
+			if(video){
+				sendJsonResponse(res, 200,video);
+			}else {
+				sendJsonResponse(res, 400, err );
+			}
 		});
 		
 	
 	};
 module.exports.videosUpdateOne = function (req, res) { 
-	//sendJsonResponse(res, 200,{"status":"success"});
+	if(!req.params.videoid){
+		sendJsonResponse(res, 404, {
+			"message": "Not found, videoid is required"
+		});
+		return;
+	}
+	Vid
+		.findById(req.params.videoid)
+		.exec(
+			function(err, video){
+				if(!video){
+					sendJsonResponse(res, 404, err);
+					return;
+				} else if(err){
+					sendJsonResponse(res, 400, err);
+					return;
+				}
+				console.log(req.body.pathUrl);
+				//sendJsonResponse(res, 200, req.body)
+				if(req.body.tipo != null ) video.tipo = req.body.tipo;
+				if(req.body.categoria != null )video.categoria = req.body.categoria;
+				if(req.body.genero != null ) video.genero = req.body.genero.split(",");
+				if(req.body.nombreSerie != null )video.nombreSerie = req.body.nombreSerie;
+				if(req.body.nombre != null )video.nombre = req.body.nombre;
+				if(req.body.thumbnail != null ) video.thumbnail = req.body.thumbnail;
+				if(req.body.pathUrl != null )video.pathUrl = req.body.pathUrl;
+				if(req.body.cast != null ) {var rCast = req.body.cast.split(",");} else var rCast = video.creditos.cast;
+				if(req.body.creador != null) {var rCreador = req.body.creador;} else var rCreador = video.creditos.creador;
+				if(req.body.director != null) {var rDirector = req.body.director;} else var rDirector = video.creditos.director;
+				if(req.body.productor != null) {var rProductor = req.body.productor;} else var rProductor = video.creditos.productor;
+				video.creditos = {
+					cast: rCast,
+					creador: rCreador,
+					director: rDirector,
+					productor: rProductor
+					};
+				video.save(function(err, video){
+					if(err){
+						sendJsonResponse(res, 404, err);
+					}else {
+						sendJsonResponse(res, 200, video);
+					}
+				});
+
+			}
+		);
+
+
 	};
-module.exports.videosDeleteOne = function (req, res) { 
-	sendJsonResponse(res, 200,{"status":"success"});
+	module.exports.videosDeleteOne = function (req, res) { 
+
+	/*if(!req.params.videoid){
+		sendJsonResponse(res, 404, {
+			"message": "Not found, videoid is required"
+		});
+		return;
+	}
+	Vid.findByIdAndRemove(req.params.videoid, function(err, video) {
+		// As always, handle any potential errors:
+		if (err) sendJsonResponse(res, 500, err);
+		if(video){
+			const response = {
+				message: "Todo successfully deleted",
+				id: video._id,
+				nombre: video.nombre
+			};
+			sendJsonResponse(res, 200, response);
+		}
+	});*/
+
+	var videoId = req.params.videoid;
+	if(videoId){
+		Vid
+			.findByIdAndRemove(videoId)
+			.exec(
+				function(err, video){
+					if(err){
+						sendJsonResponse(res, 404, err);
+						return;
+					}
+					const response = {
+						message: "Successfully deleted",
+						
+					};
+					sendJsonResponse(res, 204, response);
+				}
+			);
+	} else {
+		sendJsonResponse(res, 404, {
+			"message": "No videoid"
+		});
+	}
 	};
 
 
